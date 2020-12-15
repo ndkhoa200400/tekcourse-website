5dbd28edfdc481cbd8906544be384dba8c409cb3
levanthuanmy <levanthuanmy2000@gmail.com>
2020-12-15 20:20:19 +0700

    render category.hsb

--- routes/view.route.js
+++ routes/view.route.js
@@ -4,7 +4,7 @@ const authController = require('./../controllers/auth.controller')
 var hbs = require('hbs');
 
 const router = express.Router();
-router.use(express.static(__dirname +'public'));
+router.use(express.static(__dirname + 'public'));
 hbs.registerPartials(__dirname + '../views/partials');
 
 
@@ -14,8 +14,8 @@ router.use(authController.isLoggedIn);
 router.get('/', controller.getOverview);
 
 
-router.get('/course/create-new-course', (req, res)=>{
-  res.render('create_new_course',{
+router.get('/course/create-new-course', (req, res) => {
+  res.render('create_new_course', {
     title: "Create new course"
   })
 })
@@ -24,17 +24,29 @@ router.get('/course/:slug', controller.getCourse);
 
 
 
-router.get('/signup', (req, res)=>{
-  res.render('sign_up',{
-    title:'Sign up'
+router.get('/signup', (req, res) => {
+  res.render('sign_up', {
+    title: 'Sign up'
   })
 })
 
-router.get('/login', (req,res)=>{
-  res.render('sign_in',{
+router.get('/login', (req, res) => {
+  res.render('sign_in', {
     title: 'Login'
   });
 })
 
-
+router.get('/category', (req, res) => {
+  res.render('category', {
+    title: 'Category',
+    images: '/images/courses/img-1.jpg',
+    rating: 1000000000,
+    views: -100,
+    createdDate: 15,
+    name: 'Basic Kotlin Tutorial',
+    category: 'Mobile development',
+    teacher: 'MY toda LE',
+    price: 3
+  })
+})
 module.exports = router;
